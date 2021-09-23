d0e41790cd972eeb80fc4b1499c4545a225cdd78
Zolip2 <79993447+Zolip2@users.noreply.github.com>
2021-09-23 10:52:58 +0200

    making it good

--- page/script.js
+++ page/script.js
@@ -2,6 +2,10 @@ $('#livingroom').click(function() {
     window.location.href = "002/index.html";
 })
 
+$('#motherboard').click(function() {
+    window.location.href = "003/index.html";
+})
+
 $('#keys').click(function() {
-    window.location.href = "";
+    window.location.href = "004/index.html";
 })
